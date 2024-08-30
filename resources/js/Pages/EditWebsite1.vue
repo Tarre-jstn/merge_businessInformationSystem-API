e680b69ea0a0e333ef29c2717a0fd00310d1be21
Ciara Lei Roque <ciaralei.roque.m@gmail.com>
2024-08-30 17:51:56 +0800

    edit for each component done

--- resources/js/Pages/EditWebsite1.vue
+++ resources/js/Pages/EditWebsite1.vue
@@ -1,7 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 
 onMounted(()=>{
     function insertBreakLines(businessDetails){
@@ -21,6 +21,35 @@ onMounted(()=>{
         insertBreakLines(paragraph);
     }
 });
+
+//store default / to be changed data:
+
+const textAreas = {
+    businessName: ref('BUSINESS NAME'),
+    businessDescription: ref('CHEAP AND QUALITY FOOD'),
+    businessDetails: ref("Our food service system is designed to meet the specific needs of our customers. We ensure to avail you the cheapest high quality product in the market."),
+    homePageImage: ref('https://picsum.photos/200')
+}
+
+const editButton=ref(null);
+function edit(area){
+    editButton.value = area;
+}
+
+function save(){
+    editButton.value = false;
+}
+
+function imageUpload(event){
+    const file = event.target.files[0];
+    if (file){
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            textAreas.homePageImage.value = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+}
 </script>
 
 <template>
@@ -49,35 +78,56 @@ onMounted(()=>{
                     <a class="bg-white border border-white rounded-sm py-1 px-3">Log In</a>
                 </div>
         </div>
+
         <div class="ml-1 bg-website-main flex min-h-screen">
             <!-- edit business info -->
             <div class="mt-40 ml-8 flex-col h-1/2">
                 <div>
-                    <button class="bg-white border border-white rounded-xl p-1">Edit Text</button>
-                    <p class="text-xl text-white">BUSINESS NAME</p>
+                    <button @click="edit('businessName')" class="bg-white border border-white rounded-xl p-1">Edit Text</button>
+                    <p class="text-white text-xl font-bold">{{textAreas.businessName}}</p>
+                <div v-if="editButton==='businessName'">
+                    <textarea v-model="textAreas.businessName" class="rows-2 cols-50 border boder-black"></textarea>
+                    <button @click="save()" class="bg-white rounded-xl p-1">Save</button>
                 </div>
-                <div class="mt-5">
-                    <button class="bg-white border border-white rounded-xl p-1">Edit Text</button>
-                    <p class="font-bold text-xl text-white">CHEAP AND QUALITY FOOD</p>
-                </div>
-                <div class="mt-5">
-                    <button class="bg-white border border-white rounded-xl p-1">Edit Text</button>
-                    <p id="business-details"class="text-white">
-                        Our food service system is designed to meet the specific 
-                        needs of our customers. We ensure to avail you the cheapest and 
-                        high quality product in the market.
 
-                    </p>
+                </div>
+                <div class="mt-5">
+                    <button @click="edit('businessDescription')" class="bg-white border border-white rounded-xl p-1">Edit Text</button>
+                    <p class="font-bold text-xl text-white">{{ textAreas.businessDescription }}</p>
+                    <div v-if="editButton==='businessDescription'">
+                        <textarea v-model="textAreas.businessDescription" class="rows-2 cols-50 border boder-black"></textarea>
+                        <button @click="save()" class="bg-white rounded-xl p-1">Save</button>
+                    </div>
+                </div>
+                <div class="mt-5" >
+                    <button @click="edit('businessDetails')" class="bg-white border border-white rounded-xl p-1">Edit Text</button>
+                    <p id="business-details" class="text-white">{{ textAreas.businessDetails }} </p>
+                    <div v-if="editButton==='businessDetails'">
+                        <textarea v-model="textAreas.businessDetails" class="rows-2 cols-100 border boder-black"></textarea>
+                        <button @click="save()" class="bg-white rounded-xl p-1">Save</button>
+                    </div>
                 </div>
             
             </div>
 
             <!-- image -->
-            <div class="mt-5 ml-auto flex-grow-0 w-1/2 max-w-md">
-                <button class="bg-white border border-white rounded-xl p-1">Edit Photo</button>
-                <img src="https://picsum.photos/200" class="mt-5 w-full h-auto object-cover rounded-tl-[30px]"/>
+            <div class="mt-3 ml-auto flex-grow-0 w-1/2 max-w-md">
+                <div class="flex items-center">
+                <button @click="edit('image')" class="bg-white border border-white rounded-xl">Edit Photo</button>
+                <img :src='textAreas.homePageImage.value' class ="mt-8 w-full h-auto object-cover rounded-tl-[30px]"/>
+                <div v-if="editButton==='image'" class="bg-white">
+                    <input class="p6" type="file" @change="imageUpload"/>
+                </div>
+            </div>
             </div>
         </div>
+
+        <!-- button to next section of homepage -->
+        <div class="ml-auto z-50 fixed bottom-4 right-4">
+                    <button class="bg-white border border-black rounded-2xl p-8">
+                        <i class="fa fa-arrow-down "></i>
+                    </button>
+                </div>
     
     </AuthenticatedLayout>
 
