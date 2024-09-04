75aa87876dbb05d9b8bfd412b2a36861a15a4983
Rafael Quizon <rafael.quizon.c@gmail.com>
2024-09-04 18:37:05 +0800

    added chart.js to project

---
+++ resources/js/Components/BarChart.vue
@@ -0,0 +1,45 @@
+<script>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+export default {
+    name:'BarChart',
+    components:{ Bar },
+    data() {
+        return {
+            chartData: {
+                labels: ['January', 'February', 'March'],
+                datasets: [
+                    {
+                        label: 'Data One',
+                        backgroundColor: '#f87979',
+                        data: [40, 20, 12]
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="chart-container">
+        <Bar id="somechart" :chart-data="chartData" :options="options" :data="chartData"/>
+    </div>
+    
+</template>
+
+<style>
+.chart-container {
+  width: 50%; /* or specify a fixed width like 400px */
+  height: 400px; /* or any height you need */
+}
+</style>
--- resources/js/Pages/Chats.vue
+++ resources/js/Pages/Chats.vue
@@ -1,18 +1,26 @@
-<script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
-</script>
-
 <template>
     <Head title="Home" />
-
     <AuthenticatedLayout>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">Welcome to the Chats Page!</div>
-                </div>
-            </div>
+      <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900 dark:text-gray-100">Welcome to the Chats Page!</div>
+          </div>
         </div>
+      </div>
+      <div class="chart-container">
+        <BarChart />
+      </div>
     </AuthenticatedLayout>
-</template>
+  </template>
+  
+  <script>
+  import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+  import BarChart from '@/Components/BarChart.vue';
+  import { Head } from '@inertiajs/vue3';
+  
+  export default {
+    name: 'App',
+    components: { BarChart, AuthenticatedLayout }
+  }
+  </script>
