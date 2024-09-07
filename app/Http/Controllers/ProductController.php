db14ff43482a7aecdf14004596e79bfc25fbbc75
esteban-jett <estebanjett17@gmail.com>
2024-09-07 22:50:23 +0800

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -52,9 +52,6 @@ class ProductController extends Controller
         return response()->json(['product' => $product], 200);
     }
 
-
-
-
     public function update(Request $request, $id)
     {
         $validated = $request->validate([
@@ -87,6 +84,27 @@ class ProductController extends Controller
 
         return response()->json(['product' => $product], 200);
     }
+
+
+
+
+    //Dedicated for seniorPWD_discoubtable 
+    public function updateDiscountable(Request $request, $id)
+{
+    // Validate only the seniorPWD_discountable field
+    $validated = $request->validate([
+        'seniorPWD_discountable' => 'required|in:yes,no',
+    ]);
+
+    // Find the product
+    $product = Product::findOrFail($id);
+
+    // Update only the seniorPWD_discountable field
+    $product->seniorPWD_discountable = $validated['seniorPWD_discountable'];
+    $product->save();
+
+    return response()->json(['message' => 'Discountable status updated successfully.'], 200);
+}
 
 
 
