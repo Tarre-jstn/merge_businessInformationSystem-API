8def1e1d2c65ddad004cf5fdd001f97bc0033011
Rafael Quizon <rafael.quizon.c@gmail.com>
2024-10-28 21:52:06 +0800

    Bring Back the Profile Button

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -30,61 +30,18 @@ class ProfileController extends Controller
     /**
      * Update the user's profile information.
      */
-    // public function update(ProfileUpdateRequest $request): RedirectResponse
-    // {
-    //     $request->user()->fill($request->validated());
-
-    //     if ($request->user()->isDirty('email')) {
-    //         $request->user()->email_verified_at = null;
-    //     }
-
-    //     $request->user()->save();
-
-    //     return Redirect::route('profile.edit');
-    // }
-
-    public function update(int $id, Request $request)
+    public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $request->validate([
-            'profile_img' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'name' => 'required|string|max:255',
-            'email'=> 'required|string|lowercase|email|max:255', 
-            'address' => 'required|string|max:255',
-            'contact_number' => 'required|string|max:255'
-        ]);
+         $request->user()->fill($request->validated());
 
-        $user = User::find(Auth::id());
+         if ($request->user()->isDirty('email')) {
+             $request->user()->email_verified_at = null;
+         }
 
-       
-        if ($request->hasFile('profile_img')) {
-            // Handle the file upload
-            $image = $request->file('profile_img');
-            $imageName = time() . '.' . $image->getClientOriginalExtension();
-            
-            // Save the image to storage/app/public/business_logos
-            $image->storeAs('public/user_profile', $imageName);
-    
-            // Save the image name in the database (just the filename, not the full path)
-            $user->profile_img = $imageName;
-        }
-    
+         $request->user()->save();
 
-        // Update other business fields
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->address = $request->input('address');
-        $user->contact_number = $request->input('contact_number');
-
-        // Save the business profile
-        $user->save();
-
-        return response()->json(['success' => true]);
-       
-            Log::error('Image Upload Error: ' . $e->getMessage());
-
-            return back()->withErrors(['profile_img' => 'An error occurred while uploading the image.']);
-        
-    }
+         return Redirect::route('profile.edit');
+     }
 
     public function show(Request $request){
         return response()->json(Auth::user());
